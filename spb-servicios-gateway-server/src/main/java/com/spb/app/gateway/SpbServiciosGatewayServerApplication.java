2db29cd23e21137cf0fe6d7f9e5b82de4f8a2c6b
rolodoom <rolodoom@gmail.com>
2022-09-03 20:19:55 -0500

    Add gateway server

---
+++ spb-servicios-gateway-server/src/main/java/com/spb/app/gateway/SpbServiciosGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.spb.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbServiciosGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbServiciosGatewayServerApplication.class, args);
+	}
+
+}
---
+++ spb-servicios-gateway-server/src/test/java/com/spb/app/gateway/SpbServiciosGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spb.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbServiciosGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
