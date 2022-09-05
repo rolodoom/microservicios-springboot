3d75d81fd36aad00443578f7ba8ba0f70d44e347
rolodoom <rolodoom@gmail.com>
2022-09-05 16:48:04 -0500

    Configure slow calls

--- spb-servicio-item/src/main/java/com/spb/app/item/AppConfig.java
+++ spb-servicio-item/src/main/java/com/spb/app/item/AppConfig.java
@@ -30,10 +30,12 @@ public class AppConfig {
                                     .failureRateThreshold(50)
                                     .waitDurationInOpenState(Duration.ofSeconds(10L))
                                     .permittedNumberOfCallsInHalfOpenState(5)
+                                    .slowCallRateThreshold(50)
+                                    .slowCallDurationThreshold(Duration.ofSeconds(2L))
                                     .build())
                     .timeLimiterConfig(
                             TimeLimiterConfig.custom()
-                                    .timeoutDuration(Duration.ofSeconds(2L))
+                                    .timeoutDuration(Duration.ofSeconds(6L))
                                     .build())
                     .build();
         });
