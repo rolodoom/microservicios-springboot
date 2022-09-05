d05ea18ac5659497508a392358131584db9fa4e6
rolodoom <rolodoom@gmail.com>
2022-09-05 18:41:56 -0500

    Use TimeLimiter annotation

--- spb-servicio-item/src/main/java/com/spb/app/item/controllers/ItemController.java
+++ spb-servicio-item/src/main/java/com/spb/app/item/controllers/ItemController.java
@@ -1,6 +1,7 @@
 package com.spb.app.item.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +19,7 @@ import com.spb.app.item.models.Producto;
 import com.spb.app.item.models.service.ItemService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 public class ItemController {
@@ -40,23 +42,29 @@ public class ItemController {
         return itemService.findAll();
     }
 
-    // @GetMapping("/ver/{id}/cantidad/{cantidad}")
-    // public Item ver(@PathVariable Long id, @PathVariable Integer cantidad) {
-    // // return itemService.findById(id, cantidad)
-    // return cbFactory.create("items")
-    // .run(
-    // () -> itemService.findById(id, cantidad),
-    // e -> metodoAlternativo(id, cantidad, e));
-    // }
-
-    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item ver(@PathVariable Long id, @PathVariable Integer cantidad) {
+        // return itemService.findById(id, cantidad)
+        return cbFactory.create("items")
+                .run(
+                        () -> itemService.findById(id, cantidad),
+                        e -> metodoAlternativo(id, cantidad, e));
+    }
+
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+    @GetMapping("/ver2/{id}/cantidad/{cantidad}")
+    public Item ver2(@PathVariable Long id, @PathVariable Integer cantidad) {
         return itemService.findById(id, cantidad);
     }
 
-    public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+    @TimeLimiter(name = "items", fallbackMethod = "metodoAlternativo2")
+    @GetMapping("/ver3/{id}/cantidad/{cantidad}")
+    public CompletableFuture<Item> ver3(@PathVariable Long id, @PathVariable Integer cantidad) {
+        return CompletableFuture.supplyAsync(() -> itemService.findById(id, cantidad));
+    }
 
+    public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
         logger.info(e.getMessage());
 
         Item item = new Item();
@@ -68,6 +76,20 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return item;
+    }
+
+    public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+        logger.info(e.getMessage());
+
+        Item item = new Item();
+        Producto producto = new Producto();
+
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return CompletableFuture.supplyAsync(() -> item);
     }
 
 }
