d4d6f589a01a5c1790bb9e68584246e70b8c944a
rolodoom <rolodoom@gmail.com>
2022-09-05 14:18:05 -0500

    Gateway factory filters

--- spb-servicio-item/src/main/java/com/spb/app/item/controllers/ItemController.java
+++ spb-servicio-item/src/main/java/com/spb/app/item/controllers/ItemController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spb.app.item.models.Item;
@@ -19,7 +21,10 @@ public class ItemController {
     private ItemService itemService;
 
     @GetMapping("/listar")
-    public List<Item> listar() {
+    public List<Item> listar(@RequestParam(name = "nombre") String nombre,
+            @RequestHeader(name = "token-request") String token) {
+        System.out.println(nombre);
+        System.out.println(token);
         return itemService.findAll();
     }
 
