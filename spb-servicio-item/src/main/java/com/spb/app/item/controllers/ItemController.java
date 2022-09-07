c252f25e74a3955ba90832b27b32c991ad44e1a4
rolodoom <rolodoom@gmail.com>
2022-09-06 19:50:38 -0500

    Read configuration from controller

--- spb-servicio-item/src/main/java/com/spb/app/item/controllers/ItemController.java
+++ spb-servicio-item/src/main/java/com/spb/app/item/controllers/ItemController.java
@@ -1,13 +1,18 @@
 package com.spb.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -25,6 +30,9 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 public class ItemController {
 
     static final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+    @Value("${configuration.text}")
+    private String texto;
 
     @Autowired
     private CircuitBreakerFactory cbFactory;
@@ -90,6 +98,17 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return CompletableFuture.supplyAsync(() -> item);
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+        logger.info(texto);
+
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", texto);
+        json.put("puerto", puerto);
+
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 
 }
