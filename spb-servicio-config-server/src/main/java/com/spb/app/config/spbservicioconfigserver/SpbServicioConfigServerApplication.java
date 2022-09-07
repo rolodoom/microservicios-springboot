22b1eca4e878b125126d15a6aa04c4c4bc27a4fc
rolodoom <rolodoom@gmail.com>
2022-09-06 19:13:22 -0500

    Create config server

---
+++ spb-servicio-config-server/src/main/java/com/spb/app/config/spbservicioconfigserver/SpbServicioConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.spb.app.config.spbservicioconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpbServicioConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbServicioConfigServerApplication.class, args);
+	}
+
+}
