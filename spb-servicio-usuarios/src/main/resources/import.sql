7cb5ab4c51f293e2446a77af2280fe33bb1635d5
rolodoom <rolodoom@gmail.com>
2022-09-11 18:04:57 -0500

    Implement rest respository for CRUD

---
+++ spb-servicio-usuarios/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('rolo', '12345', 1, 'Rolando', 'Ramos','rolando@triclick.co');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin', '54321', 1, 'John', 'Doe','jhon.doe@triclick.co');
+
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
