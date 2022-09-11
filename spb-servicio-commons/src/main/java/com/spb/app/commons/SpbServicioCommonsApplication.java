7a8d486c699eefc298d075e88690c5491a0b1b94
rolodoom <rolodoom@gmail.com>
2022-09-11 18:41:37 -0500

    Create commons library

---
+++ spb-servicio-commons/src/main/java/com/spb/app/commons/SpbServicioCommonsApplication.java
@@ -0,0 +1,8 @@
+package com.spb.app.commons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbServicioCommonsApplication {
+
+}
---
+++ spb-servicio-commons/src/test/java/com/spb/app/commons/spbserviciocommons/SpbServicioCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spb.app.commons.spbserviciocommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbServicioCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
