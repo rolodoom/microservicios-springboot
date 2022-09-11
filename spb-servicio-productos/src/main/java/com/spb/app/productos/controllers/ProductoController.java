b8b31322b08197259637d75a35f1771b80b5aef0
rolodoom <rolodoom@gmail.com>
2022-09-11 12:51:23 -0500

    Add DELETE handler - eliminar

--- spb-servicio-productos/src/main/java/com/spb/app/productos/controllers/ProductoController.java
+++ spb-servicio-productos/src/main/java/com/spb/app/productos/controllers/ProductoController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,4 +67,11 @@ public class ProductoController {
 
         return productoService.save(productoDb);
     }
+
+    @DeleteMapping("/eliminar/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminar(@PathVariable Long id) {
+        productoService.deleteById(id);
+    }
+
 }
