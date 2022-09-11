971b4a3361d082cd2855d01cf8d7aa35965eb8e3
rolodoom <rolodoom@gmail.com>
2022-09-11 17:15:54 -0500

    Create servicio-usuarios

---
+++ spb-servicio-usuarios/src/main/java/com/spb/app/usuarios/SpbServicioUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.spb.app.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbServicioUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbServicioUsuariosApplication.class, args);
+	}
+
+}
---
+++ spb-servicio-usuarios/src/test/java/com/spb/app/usuarios/spbserviciousuarios/SpbServicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spb.app.usuarios.spbserviciousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbServicioUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
