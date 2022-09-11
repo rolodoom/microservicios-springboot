a3aaca96f56552005e11406fc626cbda80ad632e
rolodoom <rolodoom@gmail.com>
2022-09-11 12:39:31 -0500

    Add POST handler - crear

--- spb-servicio-productos/src/main/java/com/spb/app/productos/controllers/ProductoController.java
+++ spb-servicio-productos/src/main/java/com/spb/app/productos/controllers/ProductoController.java
@@ -6,8 +6,12 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spb.app.productos.models.entity.Producto;
@@ -43,5 +47,11 @@ public class ProductoController {
         Producto producto = productoService.findById(id);
         producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
         return producto;
+    }
+
+    @PostMapping("/crear")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Producto crear(@RequestBody Producto producto) {
+        return productoService.save(producto);
     }
 }
