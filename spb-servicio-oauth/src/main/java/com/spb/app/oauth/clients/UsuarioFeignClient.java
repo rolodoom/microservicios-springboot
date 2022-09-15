8143f1927d7fc1ce455fa6a18f73390132ebed77
rolodoom <rolodoom@gmail.com>
2022-09-15 14:57:18 -0500

    Implement UserDetailsService with Feign Client

---
+++ spb-servicio-oauth/src/main/java/com/spb/app/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,15 @@
+package com.spb.app.oauth.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.spb.app.commons.usuarios.models.entity.Usuario;
+
+@FeignClient(name = "servicio-usuarios")
+public interface UsuarioFeignClient {
+
+    @GetMapping("/usuarios/search/buscar-username")
+    public Usuario findByUsername(@RequestParam String username);
+
+}
