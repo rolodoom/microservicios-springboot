0347c59634256a0698d86e920bd5f7eebafe5057
rolodoom <rolodoom@gmail.com>
2022-09-16 12:29:30 -0500

    Add postgresql config

--- spb-servicio-usuarios/src/main/resources/import.sql
+++ spb-servicio-usuarios/src/main/resources/import.sql
@@ -1,9 +1,21 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('rolo', '12345', 1, 'Rolando', 'Ramos','rolando@triclick.co');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin', '54321', 1, 'John', 'Doe','jhon.doe@triclick.co');
+-- # MYSQL
+-- INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('rolo', '12345', 1, 'Rolando', 'Ramos','rolando@triclick.co');
+-- INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin', '54321', 1, 'John', 'Doe','jhon.doe@triclick.co');
 
-INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
-INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+-- INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+-- INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
 
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
+-- INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
+-- INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
+-- INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
+
+-- PostgreSQL
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('rolo', '12345', true, 'Rolando', 'Ramos','rolando@triclick.co');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '54321', true, 'John', 'Doe','jhon.doe@triclick.co');
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 2);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 1);
